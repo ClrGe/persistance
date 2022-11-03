3c6c6fc05d8673e2209a1bf37e03d0e3ccf5ab78
clerge <cg@jmg-conseil.eu>
2022-11-03 13:06:38 +0100

    added mongodb connection

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config({ path: './.env'});
+
+const express = require('express');
+const cors = require('cors');
+const dbo = require('./db/conn');
+
+//##############################//
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes/record'));
+
+app.use(function (err, _req, res) {
+    console.error(err.stack);
+    res.status(500).send('Error');
+});
+
+dbo.connectToServer(function (err) {
+    if (err) {
+        console.error(err);
+        process.exit();
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Server Started at ${PORT}`)
+    });
+});
+
