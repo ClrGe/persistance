60d847534b4408678025da165df2cd915fe7b998
Lokda-spec <gouarnec@gmail.com>
2022-11-07 16:23:41 +0100

    added api_key validation on http requests

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dbo = require('./db/conn');
 
 //##############################//
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors());
