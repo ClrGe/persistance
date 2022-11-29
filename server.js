d3c50db1e6d0a593308019f2f51c452859fcad82
Lokda-spec <gouarnec@gmail.com>
2022-11-29 13:25:24 +0100

    corrected indent

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ import  express     from "express"
 import  MongoClient from "mongodb"
 import  dotenv      from "dotenv"
 
-dotenv.config({ path: "./.env" });
-
 const   dataParams          = {useNewUrlParser: true, useUnifiedTopology: true},
         dataSource          = process.env.DB_URI    || "mongodb://localhost:27017",
         apiKey              = process.env.API_KEY,
@@ -23,10 +21,18 @@ let     database,
         serverStatus,
         document;
 
+//--------------------------------------------------------------------------------
+
 const app = express();
 
-// OpenAPI 3.0 documentation
+// Environment variables
+dotenv.config({ path: "./.env" });
+
+// OpenAPI 3.0 documentation (SwaggerUI)
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup("./docs/swagger.json"));
+
+
+//---------------------------------- FUNCTIONS ------------------------------------
 
 // Defining the 'log' object
 async function traceDbAccess(req, res, next) {
@@ -40,8 +46,11 @@ async function traceDbAccess(req, res, next) {
         server      : req.serverStatus,
         comment     : "",
     }
+
     next();
 }
+
+//---------------------------------- SERVER ------------------------------------
 
 // Connect to data source + requests
 async function connectDataSource() {
@@ -61,22 +70,29 @@ async function connectDataSource() {
 
         serverStatus = await db.admin().serverStatus();
 
-//------------------------------SUPERVISION--------------------------------
+        //--------------------------- SUPERVISION -----------------------------
 
         app.get("/api/status", async (req, res) => {
 
             comment = 'New connection to server ' + serverStatus.host + ' version ' + serverStatus.version;
 
             console.log(comment);
+
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 res.status(200).send(comment);
+
             } else {
+
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
+
         });
 
-//----------------------------CRUD OPERATIONS------------------------------
+        //-------------------------- CRUD OPERATIONS --------------------------
 
         // Return all documents in the collection
         app.get("/api/find/all", async (req, res) => {
@@ -89,17 +105,25 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let findResult = await db.collection(collection).find({}).toArray();
-                res.status(200).json(findResult);
                 comment = `Display all documents of collection ${collection} in database ${database}`;
+
+                res.status(200).json(findResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
+
             server = serverStatus;
             trace.insertOne(req.log);
+
         });
 
         app.post("/api/find/all", async (req, res) => {
@@ -113,14 +137,20 @@ async function connectDataSource() {
 
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let findResult = await db.collection(collection).find({}).toArray();
-                res.status(200).json(findResult);
                 comment = `Display all documents of collection ${collection} in database ${database}`;
+
+                res.status(200).json(findResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -138,14 +168,20 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
-                let findOneResult = await db.collection(collection).findOne(queryFilter)
-                res.json(findOneResult);
+                let findOneResult = await db.collection(collection).findOne(queryFilter);
                 comment = `Display document matching filter ${queryFilter} in collection ${collection} of database ${database}`;
+
+                res.json(findOneResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -163,14 +199,20 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
-                let findOneResult = await db.collection(collection).findOne(queryFilter)
-                res.json(findOneResult);
+                let findOneResult = await db.collection(collection).findOne(queryFilter);
                 comment = `Display document matching filter ${queryFilter} in collection ${collection} of database ${database}`;
+
+                res.json(findOneResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -186,17 +228,22 @@ async function connectDataSource() {
             db              = mongo.db(database);
             req.log.comment = comment;
             req.log.server  = serverStatus.host;
-            //filter        = req.body.filter.id;
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let findIdResult = await db.collection(collection).find({}).toArray();
+
                 res.json(findIdResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -211,17 +258,22 @@ async function connectDataSource() {
             db              = mongo.db(database);
             req.log.comment = comment;
             req.log.server  = serverStatus.host;
-            //filter        = req.body.filter.id;
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let findIdResult = await db.collection(collection).find({}).toArray();
+
                 res.json(findIdResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -240,18 +292,25 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let insertResult = await db.collection(collection).insertOne(document)
+
                 if(insertResult.acknowledged) {
                     comment = `Added a new document with id ${insertResult.insertedId} in database ${database}, collection ${collection}`;
                 } else {
                     comment = `Error : could not insert document in database ${database}, collection ${collection}`;
                 }
+
                 res.status(200).send(insertResult);
+
             } else {
+
                 req.authorized = false;
                 req.log.comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             req.log.deletedCount
@@ -272,13 +331,19 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let updateResult = await db.collection(collection).updateOne(document, updates);
+
                 res.status(200).json(updateResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -298,13 +363,19 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let updateResult = await db.collection(collection).updateMany(filter, updates);
+
                 res.status(200).json(updateResult);
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -323,18 +394,25 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let deleteResult = await db.collection(collection).deleteMany(filter);
+
                 res.status(200).send(deleteResult);
+
                 if(deleteResult.deletedCount >= 1){
                     comment = `${deleteResult.deletedCount} document(s) deleted in database ${database}, collection ${collection}`;
                 } else {
                     comment = `No document matching filter ${filter}. Nothing was deleted in database ${database}, collection ${collection}`;
                 }
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
+
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -352,18 +430,23 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let deleteResult = await db.collection(collection).deleteMany(filter);
                 res.status(200).send(deleteResult);
+
                 if(deleteResult.deletedCount >= 1){
                     comment = `${deleteResult.deletedCount} document(s) deleted in database ${database}, collection ${collection}`;
                 } else {
                     comment = `No document matching filter ${filter}. Nothing was deleted in database ${database}, collection ${collection}`;
                 }
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -382,18 +465,23 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let deleteResult = await db.collection(collection).deleteOne(document);
                 res.status(200).send(deleteResult);
+
                 if(deleteResult.deletedCount >= 1){
                     comment = `Document with id ${req.params.id} was deleted in database ${database}, collection  ${collection}`;
                 } else {
                     comment = `No document with id ${req.params.id}. No deletion in database ${database}, collection : ${collection}`;
                 }
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -411,6 +499,7 @@ async function connectDataSource() {
             trace           = db.collection("logs");
 
             if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
                 req.authorized = true;
                 let deleteResult = await db.collection(collection).deleteOne(document);
                 res.status(200).send(deleteResult);
@@ -419,10 +508,13 @@ async function connectDataSource() {
                 } else {
                     comment = `No document with id ${req.params.id}. No deletion in database ${database}, collection : ${collection}`;
                 }
+
             } else {
+
                 req.authorized = false;
                 comment = `Unauthorized: missing API key`;
                 res.status(401).send(comment);
+
             }
 
             trace.insertOne(req.log);
@@ -430,88 +522,92 @@ async function connectDataSource() {
         });
 
     } catch (err) {
+
         console.error(err);
+
     }
 
     // Server setup
     const server = app.listen(PORT, () => {
+
         console.log(`===================== \nServer listening on port ${PORT} \n=====================`);
         console.log(`Now connected to MongoDB instance \n===================== `);
+
     });
+
+    //-------------------------- PROCESS MANAGEMENT ---------------------------
 
     // Process termination
     process.on('SIGINT', function() {
+
         mongo.close(function () {
           console.log('Mongo disconnected on app termination');
           process.exit(0);
         });
+
     });
 
-    // setInterval(() => server.getConnections(
-    //     (err, connections) => console.log(`${connections} connections currently open`)
-    // ), 10000);
+    process.on('SIGTERM', shutDown);
+    process.on('SIGINT', shutDown);
 
-      process.on('SIGTERM', shutDown);
-      process.on('SIGINT', shutDown);
+    let connections = [];
 
-      let connections = [];
-
-      server.on('connection', connection => {
-          connections.push(connection);
-          connection.on('close', () => connections = connections.filter(curr => curr !== connection));
-      });
+    server.on('connection', connection => {
+        connections.push(connection);
+        connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+    });
 
     // Close MongoDB connection
     async  function mongoClose() {
-        mongo.close(function() {
-        console.log('Mongo disconnected on app termination');
-      })
+
+        mongo.close(function() {console.log('Mongo disconnected on app termination');});
+
     };
 
     // Kill process
     async function shutDown() {
 
-       console.log("Received kill signal, shutting down gracefully");
+        console.log("Received kill signal, shutting down gracefully");
 
-       server.close(() => {
-         console.log("Closed out remaining connections");
-         process.exit(0);
-       });
+        server.close(() => {
+            console.log("Closed out remaining connections");
+            process.exit(0);
+        });
 
-       setTimeout(() => {
-         console.error(
-           "Could not close connections in time, forcefully shutting down"
-         );
-         process.exit(1);
-       }, 10000);
+        setTimeout(() => {
+            console.error("Could not close connections in time, forcefully shutting down");
+            process.exit(1);
+        }, 10000);
+
        connections.forEach((curr) => curr.end());
        setTimeout(() => connections.forEach((curr) => curr.destroy()), 5000);
-     }
 
-     server.on("connection", (connection) => {
-       connections.push(connection);
-       connection.on(
-         "close",
-         () => (connections = connections.filter((curr) => curr !== connection))
-       );
+    }
 
-     });
+    server.on("connection", (connection) => {
+
+        connections.push(connection);
+        connection.on("close", () => (connections = connections.filter((curr) => curr !== connection)));
+
+    });
 
     // Authenticated endpoint for process termination
     app.get("/api/stop", async (req, res) => {
 
         if (req.headers.api_key & (req.headers.api_key == apiKey)) {
+
             res.status(200).json("Stopping persistance service...");
             await mongoClose().then(shutDown());
+
         } else {
+
             req.authorized = false;
             comment = `Unauthorized: missing API key`;
             res.status(401).send(comment);
+
         }
 
     });
 }
 
 connectDataSource().catch(console.error);
-
-//module.exports = app;
